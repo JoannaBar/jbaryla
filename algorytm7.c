0631ad39b7e5de06718a4f4cc465a54d93497880
JoannaBar <aj.joannaba@onet.pl>
2017-11-09 20:33:07 +0100

    MASTER ALGORYTM 7

--- algorytm7.c
+++ algorytm7.c
@@ -3,7 +3,7 @@
 // (C)2012 mgr Jerzy Wa³aszek
 //----------------------------------
 
-#include <iostream>
+#include <iostream>																			//ZMIANA MASTER
 #include <string>
 #include <cstdlib>
 #include <time.h>
