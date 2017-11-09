1558a4380c0e7030a025fa5eeb17cf6c19c463a5
unknown <student171@pcl171.autolab.local>
2017-11-09 16:14:42 +0100

    Algorytm01

---
+++ algorytm.c
@@ -0,0 +1,29 @@
+// Ci¹g arytmetyczny
+// Data   : 6.02.2011
+// (C)2012 mgr Jerzy Wa³aszek
+//----------------------------
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  int    n,i;
+  double a,d;
+
+  cout << fixed
+       << setprecision(3);
+
+  cin >> n >> a >> d;
+
+  for(i = 1; i <= n; i++)
+    cout << setw(9)
+         << a + (i - 1) * d
+         << " ";
+
+  cout << endl;
+
+  return 0;
+}
